57051096d6f62fc91e7afd3a35e2ca3032cd5053
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2021-08-20 20:50:54 +0300

    added boilerplate project

--- first-program/main.cpp
+++ first-program/main.cpp
@@ -7,6 +7,7 @@ int main() {
     std::cout << "Enter your favorite number 1 - 100: " << std::endl;
     std::cin >> favorite_number;
     std::cout << "Amazing!!! That's my favorite number two!" << std::endl;
+    std::cout << "No really!!! " << favorite_number << " is my favorite number!" << std::endl;
 
     return 0;
 }
