5ee1f208f6fba29240979fd82f2c5f526883909d
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2021-08-20 12:40:10 +0300

    added first project

--- first-program/main.cpp
+++ first-program/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
 int main() {
+
+    int favorite_number;
+
     std::cout << "Enter your favorite number 1 - 100: " << std::endl;
-    
+    std::cin >> favorite_number;
+    std::cout << "Amazing!!! That's my favorite number two!" << std::endl;
+
     return 0;
 }
